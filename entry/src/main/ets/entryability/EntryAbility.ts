7552f9e40f7197547a29088ca2e5260442aeaa91
zmtzawqlp <zmtzawqlp@live.com>
2023-11-19 22:45:00 +0800

    PullToRefreshNotification

--- entry/src/main/ets/entryability/EntryAbility.ts
+++ entry/src/main/ets/entryability/EntryAbility.ts
@@ -13,9 +13,33 @@ export default class EntryAbility extends UIAbility {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
   }
 
-  onWindowStageCreate(windowStage: window.WindowStage): void {
+  async enterImmersion(windowClass: window.Window): Promise<void> {
+    // 获取状态栏和导航栏的高度
+    windowClass.on("avoidAreaChange", ({ type, area }) => {
+      if (type == window.AvoidAreaType.TYPE_SYSTEM) {
+        // 将状态栏和导航栏的高度保存在AppStorage中
+        AppStorage.SetOrCreate<number>("topHeight", area.topRect.height);
+        AppStorage.SetOrCreate<number>("bottomHeight", area.bottomRect.height);
+      }
+    })
+    // 设置窗口布局为沉浸式布局
+    await windowClass.setWindowLayoutFullScreen(true)
+    await windowClass.setWindowSystemBarEnable(["status", "navigation"])
+    // 设置状态栏和导航栏的背景为透明
+    await windowClass.setWindowSystemBarProperties({
+      navigationBarColor: "#00000000",
+      statusBarColor: "#00000000",
+      navigationBarContentColor: "#FF0000",
+      statusBarContentColor: "#FF0000"
+    })
+  }
+
+  async onWindowStageCreate(windowStage: window.WindowStage): Promise<void> {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+
+
+    // await this.enterImmersion(await windowStage.getMainWindow());
 
     windowStage.loadContent('pages/Index', (err, data) => {
       if (err.code) {
